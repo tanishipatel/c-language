079773f84b6b3be94940c26d608b4e3c55f493ed
tanishipatel <pateltanishi746@gmail.com>
2025-07-03 18:12:52 +0530

    Add files via upload

--- first.c
+++ first.c
@@ -8,5 +8,36 @@ int main(){
     area=PI*radius*radius;
     printf("%f",area);
 
+    float length,area1;
+    printf("\n enter length:");
+    scanf("%f",&length);
+    area1=length*length;
+    printf("%f",area1);
+
+    float base,height,area2;
+    printf("enter base:");
+    scanf("%f",&base);
+    printf("enter height:");
+    scanf("%f",&height);
+    area2=(0.5*base*height);
+    print("%f",area2);
+
+    #define pi=3.14;
+    float r,perimeter;
+    printf("enter r:");
+    scanf("%f",&r);
+    perimeter=2*pi*radius;
+    printf("%f",perimeter);
+
+    float p,rate,time,si;
+    printf("enter p:");
+    scanf("%f",&p);
+    printf("enter rate:");
+    scanf("%f",&rate);
+    printf("enter time");
+    scanf("%f",&time);
+    si=(p*rate*time)/100;
+    printf("%f",si);
+    
     return 0;
 }
