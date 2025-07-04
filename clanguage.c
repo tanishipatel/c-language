eb3552dbfcfcde952de94f166658c51ca8e5121f
tanishipatel <pateltanishi746@gmail.com>
2025-07-04 10:52:31 +0530

    Add files via upload

--- clanguage.c
+++ clanguage.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-int main(){
-    float p,rate,time,SI;
-    printf("enter p:");
-    scanf("%f",&p);
-    printf("enter rate:");
-    scanf("%f",&rate);
-    printf("enter time:");
-    scanf("%f",&time);
-    SI=(p*rate*time)/100;
-    printf("\n%f",SI);
-    
+int main() {
+    printf("add of 12 and 6 is 18");
+    printf("\n sub of 12 and 6 is 6");
+    printf("\n product of 12 and 6 is 72");
+    printf("\n division of 12 and 6 is 2");
+    printf("\n modulo of 12 and 6 is 0");
+    printf("\n 12+6=%d",12+6);
+    printf("\n 12-6=%d",12-6);
+    printf("\n 12*6=%d",12*6);
+    printf("\n 12/6=%d",12/6);
+    printf("\n 12%6=%d",12%6);
+
+    printf("---------");
+    printf("\n|       |");
+    printf("\n|5x5= %d|",5*5);
+    printf("\n|       |");
+    printf("\n---------");
+
     return 0;
+}
+
+    
